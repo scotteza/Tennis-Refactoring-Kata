fb48244bb15a83810ba498f81d27c098635c6c3a
Scott Edwards <scotteza@gmail.com>
2018-07-27 14:25:23 +0100

    Added temporary ScoreHelper class to refactor to-stringing scores.

--- csharp/Tennis/ScoreHandlers/NormalCaseScoreHandler.cs
+++ csharp/Tennis/ScoreHandlers/NormalCaseScoreHandler.cs
@@ -14,27 +14,10 @@ namespace Tennis.ScoreHandlers
 
         public string GetScore(Scores scores)
         {
-            var score1Description = GetScoreDescription(scores.Score1);
-            var score2Description = GetScoreDescription(scores.Score2);
+            var score1Description = ScoreHelper.GetScoreDescription(scores.Score1);
+            var score2Description = ScoreHelper.GetScoreDescription(scores.Score2);
 
             return $"{score1Description}-{score2Description}";
-        }
-
-        private static string GetScoreDescription(int score)
-        {
-            switch (score)
-            {
-                case 0:
-                    return "Love";
-                case 1:
-                    return "Fifteen";
-                case 2:
-                    return "Thirty";
-                case 3:
-                    return "Forty";
-            }
-
-            throw new ScoreDescriptionNotFoundException($"Score description not found for score {score}");
         }
     }
 }
---
+++ csharp/Tennis/ScoreHelper.cs
@@ -0,0 +1,25 @@
+﻿using Tennis.Exceptions;
+
+namespace Tennis
+{
+    // TODO: this is a temporary step, this class will be refactored (removed) later
+    public static class ScoreHelper
+    {
+        public static string GetScoreDescription(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+            }
+
+            throw new ScoreDescriptionNotFoundException($"Score description not found for score {score}");
+        }
+    }
+}
