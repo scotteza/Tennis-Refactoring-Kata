a3889e0a38cf070fae459ed5db533e43e8097fa6
Scott Edwards <scotteza@gmail.com>
2018-07-27 14:02:34 +0100

    Refactored InAdvantageAndWinRangeScoreHandler.

--- csharp/Tennis/ScoreHandlers/InAdvantageAndWinRangeScoreHandler.cs
+++ csharp/Tennis/ScoreHandlers/InAdvantageAndWinRangeScoreHandler.cs
@@ -1,31 +1,52 @@
-﻿namespace Tennis.ScoreHandlers
+﻿using System;
+
+namespace Tennis.ScoreHandlers
 {
     internal class InAdvantageAndWinRangeScoreHandler : IScoreHandler
     {
+        private const int MinimumScoreForAdvantageOrWin = 4;
+        private const int MinimumScoreDifferenceForWin = 2;
+
         public bool CanHandle(Scores scores)
         {
-            return scores.Score1 >= 4 || scores.Score2 >= 4;
+            return scores.Score1 >= MinimumScoreForAdvantageOrWin
+                   || scores.Score2 >= MinimumScoreForAdvantageOrWin;
         }
 
         public string GetScore(Scores scores)
         {
-            var minusResult = scores.Score1 - scores.Score2;
-            if (minusResult == 1)
-            {
-                return "Advantage player1";
-            }
-            else if (minusResult == -1)
-            {
-                return "Advantage player2";
-            }
-            else if (minusResult >= 2)
-            {
-                return "Win for player1";
-            }
-            else
-            {
-                return "Win for player2";
-            }
+            var minusResult = GetAdvantageOrWin(scores);
+
+            return minusResult == AdvantageOrWin.Advantage
+                ? Advantage(scores)
+                : Win(scores);
+        }
+
+        private static AdvantageOrWin GetAdvantageOrWin(Scores scores)
+        {
+            return Math.Abs(scores.Score1 - scores.Score2) >= MinimumScoreDifferenceForWin
+                ? AdvantageOrWin.Win
+                : AdvantageOrWin.Advantage;
+        }
+
+        private static string Advantage(Scores scores)
+        {
+            return scores.Score1 > scores.Score2
+                ? "Advantage player1"
+                : "Advantage player2";
+        }
+
+        private static string Win(Scores scores)
+        {
+            return scores.Score1 > scores.Score2
+                ? "Win for player1"
+                : "Win for player2";
+        }
+
+        private enum AdvantageOrWin
+        {
+            Advantage,
+            Win
         }
     }
 }
