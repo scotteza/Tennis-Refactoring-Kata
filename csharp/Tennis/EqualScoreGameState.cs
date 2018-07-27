a277ae09c7bcaf983c045366c6372f7169daebb7
Scott Edwards <scotteza@gmail.com>
2018-07-27 12:32:11 +0100

    Game states moved to their own classes.

---
+++ csharp/Tennis/EqualScoreGameState.cs
@@ -0,0 +1,24 @@
+﻿namespace Tennis
+{
+    internal class EqualScoreGameState : GameState
+    {
+        public override string GetScore()
+        {
+            switch (player1.Score)
+            {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                default:
+                    return "Deuce";
+            }
+        }
+
+        public EqualScoreGameState(Player player1, Player player2) : base(player1, player2)
+        {
+        }
+    }
+}
---
+++ csharp/Tennis/InAdvantageAndWinRangeGameState.cs
@@ -0,0 +1,30 @@
+﻿namespace Tennis
+{
+    internal class InAdvantageAndWinRangeGameState : GameState
+    {
+        public InAdvantageAndWinRangeGameState(Player player1, Player player2) : base(player1, player2)
+        {
+        }
+
+        public override string GetScore()
+        {
+            var minusResult = player1.Score - player2.Score;
+            if (minusResult == 1)
+            {
+                return "Advantage player1";
+            }
+            else if (minusResult == -1)
+            {
+                return "Advantage player2";
+            }
+            else if (minusResult >= 2)
+            {
+                return "Win for player1";
+            }
+            else
+            {
+                return "Win for player2";
+            }
+        }
+    }
+}
